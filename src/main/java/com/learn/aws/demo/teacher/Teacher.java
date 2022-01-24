3f6e9e1cc14eaef90e30bc22680c6f8b2d7ac5f4
rajajeswanth <97299232+rajajeswanth@users.noreply.github.com>
2022-01-24 13:30:37 -0600

    Create Teacher.java

---
+++ src/main/java/com/learn/aws/demo/teacher/Teacher.java
@@ -0,0 +1,22 @@
+package com.learn.aws.demo.teacher;
+
+public class Teacher {
+  String name;
+  String[] students;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String[] getStudents() {
+    return students;
+  }
+
+  public void setStudents(String[] students) {
+    this.students = students;
+  }
+}
