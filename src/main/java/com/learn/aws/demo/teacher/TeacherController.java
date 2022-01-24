6f3b17ce421afb83d02885c5f04b3f3529302148
rajajeswanth <97299232+rajajeswanth@users.noreply.github.com>
2022-01-24 13:31:32 -0600

    Add files via upload

---
+++ src/main/java/com/learn/aws/demo/teacher/TeacherController.java
@@ -0,0 +1,26 @@
+package com.learn.aws.demo.teacher;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController(value = "TeacherController")
+@RequestMapping("/teacher")
+public class TeacherController {
+  @GetMapping("/getStudents")
+  ResponseEntity<Teacher> getStudents() {
+//    String studentsUrl = "http://student.rajaaws:8080/student/getStudents";
+//    RestTemplate restTemplate = new RestTemplate();
+//    ResponseEntity<Object> responseEntity = restTemplate.getForEntity(studentsUrl, Object.class);
+
+    Object students = new String[] {"Raja", "Kirti"};
+    Teacher teacher = new Teacher();
+    teacher.setName("Science Teacher");
+//    teacher.setStudents((String[])responseEntity.getBody());
+    teacher.setStudents((String[])students);
+    return new ResponseEntity<>(teacher, HttpStatus.OK);
+  }
+}
